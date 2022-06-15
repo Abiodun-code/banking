3ad76bdca346149eac9f5b9127ea867d70f8fadf
Abiodun-code <olaabiodun157@gmail.com>
2022-06-14 23:58:55 -0800

    App-change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './index.css';
 import './query.css'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Body from './components/Body';
